50e19a03eab8edbc2ac0b86f6c2a62b9f16fb011
Pedro Nhamirre <pedrooliv62@gmail.com>
2023-04-09 12:26:43 +0200

    Update ConexaoDAO.java

--- HR/src/java/DAO/ConexaoDAO.java
+++ HR/src/java/DAO/ConexaoDAO.java
@@ -17,7 +17,7 @@ public class ConexaoDAO {
         Connection conexao = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/bdGestaoRH?user=root&password=B@dF0x16";
+            String url = "jdbc:mysql://localhost:3306/bdGestaoRH?user=root";
             conexao = DriverManager.getConnection(url);
 
         } catch (SQLException erro) {
